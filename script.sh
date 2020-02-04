ac9a912cff47f4cec539e940f68c2dd7cf10f49d
Kulbhushan Mayer <kulbhushan.mayer@gmail.com>
2020-02-04 16:33:49 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,3 +2,4 @@
 
 echo "[INFO] This is for GIT and UCD Integration for Artefacts Versioning"
 echo "[INFO] This is the second change for automated version import"
+echo "[INFO] This is the third change for Automate Version Deployment after Version Import"
